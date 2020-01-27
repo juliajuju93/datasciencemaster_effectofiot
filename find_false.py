fc1738a60ae4df2ca5686d24cadd3e373c8c3ebc
juliajuju93 <42241691+juliajuju93@users.noreply.github.com>
2020-01-27 12:37:44 +0100

    Update find_false.py

--- find_false.py
+++ find_false.py
@@ -1,6 +1,6 @@
 import os
 import json
-img_folder_path = 'C:/Masterarbeit/No_Score_9/'
+img_folder_path = 'C:/Masterarbeit/No_Score/'
 path = img_folder_path
 files = []
 i = 0
@@ -18,17 +18,3 @@ for r, d, f in os.walk(path):
                 else:
                     print('thingID is unknown.')
                                 
-
-
-#for r, d, f in os.walk(path):
-    #for file in f:
-        #if 'trips.json' in file:
-            #import json
-            #joined_path = os.path.join(r,file)
-            #with open(joined_path) as read_file:
-                #datatime = json.load(read_file)
-                #account_list = datatime.get('accountId', '')
-                #print(str(datatime.get('accountId')))
-
-                #os.remove(os.path.join(r, file))
-
